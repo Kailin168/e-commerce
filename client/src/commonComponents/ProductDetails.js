991ef045c23096028db13ef7c54895b412c07e24
Kai L <kailin8591@gmail.com>
2022-08-23 21:17:40 -0400

    finished Auth and useContext

--- client/src/commonComponents/ProductDetails.js
+++ client/src/commonComponents/ProductDetails.js
@@ -5,12 +5,17 @@ function ProductDetails() {
   return (
     <div style={{
       display: "flex",
-      background: "green"
+      background: "green",
+      flexDirection: 'column'
     }}>
-      <div>
+      <div style={{
+        display: "flex"
+      }}>
         <div style={{
+          display: 'flex',
           flex: 5,
-          background: "blue"
+          background: "blue",
+          flexDirection: 'column'
         }}>
           image
         </div>
@@ -20,12 +25,25 @@ function ProductDetails() {
           flex: 1,
           background: "pink"
         }}>
-          <div style={{background:"red"}}>cart</div>
-          <div style={{background:"purple"}}>description</div>
+          <div style={{
+            background: "red",
+            display: 'flex',
+            flex: 5,
+          }}>
+            cart
+          </div>
+          <div style={{
+            background: "purple", display: 'flex',
+            flex: 1,
+          }}>
+            description
+          </div>
         </div>
       </div>
-      <div style={{background:"orange", display: "flex",
-    flexDirection:"row"}}>
+      <div style={{
+        background: "orange", display: "flex",
+        flexDirection: "row"
+      }}>
         reviews
       </div>
     </div>
