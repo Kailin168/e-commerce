01b987cde6e413f872dfd5f6737ea1f479ccb27d
Himanshu Vaidya <hnic07@gmail.com>
2022-08-24 14:00:10 -0400

    homepage/footer

--- client/src/Pages/Footer.js
+++ client/src/Pages/Footer.js
@@ -6,34 +6,60 @@ import { Link } from "react-router-dom";
 import Typography from '@mui/material/Typography';
 import MapIcon from '@mui/icons-material/Map';
 import InfoIcon from '@mui/icons-material/Info';
+import LocationOnSharpIcon from '@mui/icons-material/LocationOnSharp';
+import PhoneAndroidSharpIcon from '@mui/icons-material/PhoneAndroidSharp';
+import MailSharpIcon from '@mui/icons-material/MailSharp';
 
 function Footer() {
   return (
-    <div >
 
-      <AppBar position="static" elevation={0} component="footer" color="default">
-        <Grid style={{margin: "20px"}}>
-          <Grid container item sm={6} xs={11} justify="space-between">
-            <Grid item sm={5} xs={12}>
-              <MapIcon color="action" />
-              <Typography paragraph>
-                {/* <Link to="https://www.google.com/maps/place/11+Broadway,+New+York,+NY+10004/@40.7052717,-74.0139684,17z/data=!3m1!4b1!4m5!3m4!1s0x89c25a1160e06645:0x223402a1f64f844f!8m2!3d40.7052717!4d-74.0139684" target="_blank" alt="<MAP>">Find Us</Link>. */}
-                <Link to="/map">Map</Link>
-              </Typography>
-            </Grid>
-            <Grid item sm={5} xs={11}>
-              <InfoIcon color="action" />
-              <Typography paragraph>
-                For more Information, Please visit<Link to="/about">About</Link>.
-              </Typography>
-            </Grid>
-          </Grid>
-        </Grid>
-        <Toolbar style={{ justifyContent: "center" }}>
-          <Typography variant="caption">VeganMarket©2022</Typography>
-        </Toolbar>
-      </AppBar>
+    <footer className="footer-distributed">
+    <div className="footer-left">
+      <h3>VeganMarket</h3>
+      <p className="footer-links">
+        <a href="#" className="link-1">
+        <Typography paragraph>
+        {/* <Link to="https://www.google.com/maps/place/11+Broadway,+New+York,+NY+10004/@40.7052717,-74.0139684,17z/data=!3m1!4b1!4m5!3m4!1s0x89c25a1160e06645:0x223402a1f64f844f!8m2!3d40.7052717!4d-74.0139684" target="_blank" alt="<MAP>">Find Us</Link>. */}
+        <Link to="/map">Map</Link>
+      </Typography>
+        </a>
+        <a href="#">Products</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </p>
+      <p className="footer-company-name">Vegan Market © 2022</p>
     </div>
+    <div className="footer-center">
+      <div>
+        <LocationOnSharpIcon />
+        <p>
+          <span>11 Broadway</span> New York, NY 10004
+        </p>
+      </div>
+      <div>
+        <PhoneAndroidSharpIcon />
+        <p>555-555-5555</p>
+      </div>
+      <div>
+        <MailSharpIcon />
+        <p>
+          <a href="mailto:support@company.com">support@donotcontactus.com</a>
+        </p>
+      </div>
+    </div>
+    <div className="footer-right">
+      <p className="footer-company-about">
+        <span>About the Company</span>
+        A busy lifestyle usually translates to less time to shop and cook for
+        ourselves, especially with real, healthy ingredients. VeganMaket can help
+        you lead a longer, healthier, and happier life. Learning the benefits of a
+        plant-based lifestyle is only the beginning.
+      </p>
+     
+    </div>
+  </footer>
+  
+     
   )
 }
 export default Footer;
---
+++ client/src/Pages/Slider.js
@@ -0,0 +1,494 @@
+import { sliderClasses } from "@mui/material";
+import React from "react";
+
+
+function Slider() {
+
+    return(
+   
+<div
+  id="shopify-section-162826213599eb9fcf"
+  className="shopify-section home-images-slideshow-sections"
+>
+  <section className="home_slide_icons">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12">
+          <h2>
+            The largest selection of vegan products online{" "}
+            <a href="/collections/just-added" className="whapp-just-added">
+              shop our newest products
+            </a>
+          </h2>
+          <ul>
+            <li>
+              <a href="/collections/just-added">New In</a>
+            </li>
+            <li>
+              <a href="/collections/promotions">Sale</a>
+            </li>
+            <li>
+              <a href="/collections/vegan-groceries">Bestsellers</a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-lg-12 init-slider">
+          <div
+            id="new_home_slider"
+            className="slick-initialized slick-slider slick-dotted"
+            role="toolbar"
+          >
+            <button
+              type="button"
+              className="hms_arw nhs_prev slick-arrow"
+              style={{ display: "inline-block" }}
+            />
+            <div aria-live="polite" className="slick-list draggable">
+              <div
+                className="slick-track"
+                style={{ opacity: 1, width: 5229 }}
+                role="listbox"
+              >
+                <div
+                  className="slide-wrapper slick-slide slick-current slick-active"
+                  data-slick-index={0}
+                  aria-hidden="false"
+                  tabIndex={-1}
+                  role="option"
+                  aria-describedby="slick-slide20"
+                  style={{
+                    width: 747,
+                    position: "relative",
+                    left: 0,
+                    top: 0,
+                    zIndex: 999,
+                    opacity: 1
+                  }}
+                >
+                  <div className="slide_content">
+                    <a
+                      href="/collections/vegan-cheese"
+                      className="whapp-just-added"
+                      tabIndex={0}
+                    >
+                      <img
+                        className="mobile_off"
+                        alt="Plant Based Community, Lifestyle, Food"
+                        src="//cdn.shopify.com/s/files/1/0587/2045/2783/files/D_Vegan_Cheese.jpg?v=1659449648"
+                      />
+                      <img
+                        src="//cdn.shopify.com/s/files/1/0587/2045/2783/files/M_Vegan_Cheese.jpg?v=1659449656"
+                        className="mobile_on"
+                      />
+                    </a>
+                  </div>
+                </div>
+                <div
+                  className="slide-wrapper slick-slide"
+                  data-slick-index={1}
+                  aria-hidden="true"
+                  tabIndex={-1}
+                  role="option"
+                  aria-describedby="slick-slide21"
+                  style={{
+                    transition: "opacity 500ms linear 0s",
+                    width: 747,
+                    position: "relative",
+                    left: "-747px",
+                    top: 0,
+                    zIndex: 998,
+                    opacity: 0
+                  }}
+                >
+                  <div className="slide_content">
+                    <a
+                      href="/collections/vegan-candy-chocolate"
+                      className="whapp-just-added"
+                      tabIndex={-1}
+                    >
+                      <img
+                        className="mobile_off"
+                        alt="Plant Based Community, Lifestyle, Food"
+                        src="//cdn.shopify.com/s/files/1/0587/2045/2783/files/D_Sweet_Snacks.jpg?v=1659449456"
+                      />
+                      <img
+                        src="//cdn.shopify.com/s/files/1/0587/2045/2783/files/M_Sweet_Snacks.jpg?v=1659449475"
+                        className="mobile_on"
+                      />
+                    </a>
+                  </div>
+                </div>
+                <div
+                  className="slide-wrapper slick-slide"
+                  data-slick-index={2}
+                  aria-hidden="true"
+                  tabIndex={-1}
+                  role="option"
+                  aria-describedby="slick-slide22"
+                  style={{
+                    transition: "opacity 500ms linear 0s",
+                    width: 747,
+                    position: "relative",
+                    left: "-1494px",
+                    top: 0,
+                    zIndex: 998,
+                    opacity: 0
+                  }}
+                >
+                  <div className="slide_content">
+                    <a
+                      href="/collections/vegan-jerky"
+                      className="whapp-just-added"
+                      tabIndex={-1}
+                    >
+                      <img
+                        className="mobile_off"
+                        alt="Plant Based Community, Lifestyle, Food"
+                        src="//cdn.shopify.com/s/files/1/0587/2045/2783/files/D_Vegan_Jerky.jpg?v=1659449580"
+                      />
+                      <img
+                        src="//cdn.shopify.com/s/files/1/0587/2045/2783/files/M_Vegan_Jerky.jpg?v=1659449602"
+                        className="mobile_on"
+                      />
+                    </a>
+                  </div>
+                </div>
+                <div
+                  className="slide-wrapper slick-slide"
+                  data-slick-index={3}
+                  aria-hidden="true"
+                  tabIndex={-1}
+                  role="option"
+                  aria-describedby="slick-slide23"
+                  style={{
+                    transition: "opacity 500ms linear 0s",
+                    width: 747,
+                    position: "relative",
+                    left: "-2241px",
+                    top: 0,
+                    zIndex: 998,
+                    opacity: 0
+                  }}
+                >
+                  <div className="slide_content">
+                    <a
+                      href="/collections/chomp"
+                      className="whapp-just-added"
+                      tabIndex={-1}
+                    >
+                      <img
+                        className="mobile_off"
+                        alt="Plant Based Community, Lifestyle, Food"
+                        src="//cdn.shopify.com/s/files/1/0587/2045/2783/files/D_CHOMP.jpg?v=1659449691"
+                      />
+                      <img
+                        src="//cdn.shopify.com/s/files/1/0587/2045/2783/files/M_CHOMP.jpg?v=1659449706"
+                        className="mobile_on"
+                      />
+                    </a>
+                  </div>
+                </div>
+                <div
+                  className="slide-wrapper slick-slide"
+                  data-slick-index={4}
+                  aria-hidden="true"
+                  tabIndex={-1}
+                  role="option"
+                  aria-describedby="slick-slide24"
+                  style={{
+                    transition: "opacity 500ms linear 0s",
+                    width: 747,
+                    position: "relative",
+                    left: "-2988px",
+                    top: 0,
+                    zIndex: 998,
+                    opacity: 0
+                  }}
+                >
+                  <div className="slide_content">
+                    <a
+                      href="/collections/vegan-chips"
+                      className="whapp-just-added"
+                      tabIndex={-1}
+                    >
+                      <img
+                        className="mobile_off"
+                        alt="Plant Based Community, Lifestyle, Food"
+                        src="//cdn.shopify.com/s/files/1/0587/2045/2783/files/D_Savory_Snacks.jpg?v=1659449749"
+                      />
+                      <img
+                        src="//cdn.shopify.com/s/files/1/0587/2045/2783/files/M_Savory_Snacks.jpg?v=1659449764"
+                        className="mobile_on"
+                      />
+                    </a>
+                  </div>
+                </div>
+                <div
+                  className="slide-wrapper slick-slide"
+                  data-slick-index={5}
+                  aria-hidden="true"
+                  tabIndex={-1}
+                  role="option"
+                  aria-describedby="slick-slide25"
+                  style={{
+                    transition: "opacity 500ms linear 0s",
+                    width: 747,
+                    position: "relative",
+                    left: "-3735px",
+                    top: 0,
+                    zIndex: 998,
+                    opacity: 0
+                  }}
+                >
+                  <div className="slide_content">
+                    <a
+                      href="/collections/vegan-beverages"
+                      className="whapp-just-added"
+                      tabIndex={-1}
+                    >
+                      <img
+                        className="mobile_off"
+                        alt="Plant Based Community, Lifestyle, Food"
+                        src="//cdn.shopify.com/s/files/1/0587/2045/2783/files/D_Refreshing_Beverages.jpg?v=1659449891"
+                      />
+                      <img
+                        src="//cdn.shopify.com/s/files/1/0587/2045/2783/files/M_Refreshing_Beverages.jpg?v=1659449898"
+                        className="mobile_on"
+                      />
+                    </a>
+                  </div>
+                </div>
+                <div
+                  className="slide-wrapper slick-slide"
+                  data-slick-index={6}
+                  aria-hidden="true"
+                  tabIndex={-1}
+                  role="option"
+                  aria-describedby="slick-slide26"
+                  style={{
+                    transition: "opacity 500ms linear 0s",
+                    width: 747,
+                    position: "relative",
+                    left: "-4482px",
+                    top: 0,
+                    zIndex: 998,
+                    opacity: 0
+                  }}
+                >
+                  <div className="slide_content">
+                    <a
+                      href="/collections/daring"
+                      className="whapp-just-added"
+                      tabIndex={-1}
+                    >
+                      <img
+                        className="mobile_off"
+                        alt="Plant Based Community, Lifestyle, Food"
+                        src="//cdn.shopify.com/s/files/1/0587/2045/2783/files/D_Daring_Banner.jpg?v=1657292208"
+                      />
+                      <img
+                        src="//cdn.shopify.com/s/files/1/0587/2045/2783/files/M_Daring_Banner.jpg?v=1657292226"
+                        className="mobile_on"
+                      />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <button
+              type="button"
+              className="hms_arw nhs_next slick-arrow"
+              style={{ display: "inline-block" }}
+            />
+            <ul
+              className="slick-dots"
+              style={{ display: "block" }}
+              role="tablist"
+            >
+              <li
+                className="slick-active"
+                aria-hidden="false"
+                role="presentation"
+                aria-selected="true"
+                aria-controls="navigation20"
+                id="slick-slide20"
+              >
+                <button
+                  type="button"
+                  data-role="none"
+                  role="button"
+                  tabIndex={0}
+                >
+                  1
+                </button>
+              </li>
+              <li
+                aria-hidden="true"
+                role="presentation"
+                aria-selected="false"
+                aria-controls="navigation21"
+                id="slick-slide21"
+                className=""
+              >
+                <button
+                  type="button"
+                  data-role="none"
+                  role="button"
+                  tabIndex={0}
+                >
+                  2
+                </button>
+              </li>
+              <li
+                aria-hidden="true"
+                role="presentation"
+                aria-selected="false"
+                aria-controls="navigation22"
+                id="slick-slide22"
+                className=""
+              >
+                <button
+                  type="button"
+                  data-role="none"
+                  role="button"
+                  tabIndex={0}
+                >
+                  3
+                </button>
+              </li>
+              <li
+                aria-hidden="true"
+                role="presentation"
+                aria-selected="false"
+                aria-controls="navigation23"
+                id="slick-slide23"
+                className=""
+              >
+                <button
+                  type="button"
+                  data-role="none"
+                  role="button"
+                  tabIndex={0}
+                >
+                  4
+                </button>
+              </li>
+              <li
+                aria-hidden="true"
+                role="presentation"
+                aria-selected="false"
+                aria-controls="navigation24"
+                id="slick-slide24"
+                className=""
+              >
+                <button
+                  type="button"
+                  data-role="none"
+                  role="button"
+                  tabIndex={0}
+                >
+                  5
+                </button>
+              </li>
+              <li
+                aria-hidden="true"
+                role="presentation"
+                aria-selected="false"
+                aria-controls="navigation25"
+                id="slick-slide25"
+                className=""
+              >
+                <button
+                  type="button"
+                  data-role="none"
+                  role="button"
+                  tabIndex={0}
+                >
+                  6
+                </button>
+              </li>
+              <li
+                aria-hidden="true"
+                role="presentation"
+                aria-selected="false"
+                aria-controls="navigation26"
+                id="slick-slide26"
+                className=""
+              >
+                <button
+                  type="button"
+                  data-role="none"
+                  role="button"
+                  tabIndex={0}
+                >
+                  7
+                </button>
+              </li>
+            </ul>
+          </div>
+          <div className="mb_shop_now">
+            <a href="/collections/pantry" className="whapp-just-added">
+              SHOP NOW
+            </a>
+          </div>
+        </div>
+        <div className="col-lg-4 col-sm-8 col-sm-12 slide-icons d-none">
+          <div className="row">
+            <div className="col-6">
+              <div className="shop-icon __icon_grocery">
+                <a href="/pages/vegan-foods" className="whapp-just-added">
+                  <img
+                    src="//cdn.shopify.com/s/files/1/0587/2045/2783/t/21/assets/grocery.svg?v=27778989837231738491646157294"
+                    alt="Plant Based Community, Lifestyle, Food"
+                  />
+                  <span>Plant-Based Groceries</span>
+                </a>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="shop-icon __icon_xvip">
+                <a
+                  href="https://plantx.com/pages/xvip"
+                  className="whapp-just-added"
+                >
+                  <img
+                    src="//cdn.shopify.com/s/files/1/0587/2045/2783/t/21/assets/XVIP-Icon.svg?47354"
+                    alt="Plant Based Community, Lifestyle, Food"
+                  />
+                  <span>Join Now</span>
+                </a>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="shop-icon __icon_beauty">
+                <a
+                  href="https://plantx.com/collections/milk-cream"
+                  className="whapp-just-added"
+                >
+                  <span>Plant-Based Beverages</span>
+                </a>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="shop-icon __icon_xpets">
+                <a href="/collections/pets" className="whapp-just-added">
+                  <img
+                    src="//cdn.shopify.com/s/files/1/0587/2045/2783/t/21/assets/xpets.svg?v=120042687650255342031646157434"
+                    alt="Plant Based Community, Lifestyle, Food"
+                  />
+                  <span>Pet Products</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+
+    )
+}
+
+
+export default Slider
